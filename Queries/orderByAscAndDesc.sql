3709dc80a905bf51a490258c0b4a6740f7c949ba
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-07 13:09:07 +0300

    DB projects are prepared

---
+++ Queries/orderByAscAndDesc.sql
@@ -0,0 +1,16 @@
+/*ASC==Used for sorting ascending order, upper or lowercase does not matter, by default sorts by ascending */
+/*DESC==Used for sorting descending order, upper or lowercase does not matter
+  order statement needs to be at the very and after the condition*/
+
+select *
+from COUNTRIES
+order by REGION_ID;
+
+select *
+from EMPLOYEES
+order by SALARY;
+
+select * from EMPLOYEES where SALARY>= 3000 and SALARY<=4000 order by SALARY;/*By default asc*/
+select * from EMPLOYEES where SALARY>= 3000 and SALARY<=4000 order by SALARY desc;
+
+
--- Queries/whereConditions.sql
+++ Queries/whereConditions.sql
@@ -3,6 +3,7 @@ select * from EMPLOYEES where SALARY<5000 and MANAGER_ID=114;
 
 select * from LOCATIONS where LOCATION_ID between 1800 and 2000;
 select * from EMPLOYEES where SALARY between 3000 and 5000;
+select * from EMPLOYEES where SALARY>= 3000 and SALARY<=4000;
 
 /*"in" == only brings the given statements/range*/
 select * from EMPLOYEES where DEPARTMENT_ID in (30);
