33b92ada562af6728442afc026b34f48a1d96b7c
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-07 12:17:29 +0300

    DB projects are prepared

--- Queries/first.sql
+++ Queries/first.sql
@@ -12,3 +12,19 @@ select EMPLOYEE_ID, DEPARTMENTS.DEPARTMENT_ID from DEPARTMENTS, EMPLOYEES;
 
 select distinct SALARY from EMPLOYEES;
 select  DISTINCT MAX_SALARY from JOBS;
+
+/*select * from employees;
+-- reads all column from employees table
+select * from departments;
+-- reads all column from departments table
+select first_name from employees;
+--get only firstname from employees table
+--display city names
+select city from locations;
+--get me first_name,last_name and salary
+select first_name,last_name,salary from employees;
+--get me street address and postal code
+SELECT street_address, postal_code
+FROM locations;
+select distinct first_name from employees;
+--remove duplicates*/
