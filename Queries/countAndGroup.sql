f5bc36ba685cde28c16db3f20f65f187efa3149a
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-07 16:02:39 +0300

    DB projects are prepared

--- Queries/countAndGroup.sql
+++ Queries/countAndGroup.sql
@@ -1,5 +1,25 @@
+/*usually count is used with condition*/
 select count(*) from EMPLOYEES;
 
 
 select COUNTRY_NAME from COUNTRIES;
 select count(COUNTRY_NAME) from COUNTRIES where COUNTRY_NAME like 'I%';
+select count(COUNTRY_NAME) from COUNTRIES where COUNTRY_NAME like '%a%';
+select count(COUNTRY_NAME) from COUNTRIES where COUNTRY_NAME like 'A%' and COUNTRY_NAME like '%a';
+
+select  count(*) from EMPLOYEES where MANAGER_ID=100;
+select count(*) from EMPLOYEES where SALARY<10000;
+
+select min(LOCATION_ID) from DEPARTMENTS;
+select * from DEPARTMENTS where LOCATION_ID=( select min(LOCATION_ID) from DEPARTMENTS );
+select * from DEPARTMENTS where LOCATION_ID=1400;
+
+select max(LOCATION_ID) from DEPARTMENTS;
+select * from DEPARTMENTS where LOCATION_ID=( select max(LOCATION_ID) from DEPARTMENTS );
+select * from DEPARTMENTS where LOCATION_ID=2700;
+
+select min(salary) from employees;
+select max(salary) from employees;
+select avg(salary) from employees;
+select round(avg(salary),2) from employees;
+select sum(salary) from employees;
