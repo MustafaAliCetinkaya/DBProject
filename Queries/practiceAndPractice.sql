2caa87ffb4617a1472d15d4399d6675b8a9cd4c6
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-08 23:35:37 +0300

    DB projects are prepared

--- Queries/practiceAndPractice.sql
+++ Queries/practiceAndPractice.sql
@@ -1,3 +1,9 @@
 select JOB_ID, count(*), sum(SALARY), avg(SALARY)
 from EMPLOYEES
 group by JOB_ID order by avg(SALARY) desc;
+
+select JOB_ID, count(*),avg(SALARY)
+from EMPLOYEES
+group by JOB_ID
+having avg(SALARY)>6000
+order by avg(SALARY) desc;
