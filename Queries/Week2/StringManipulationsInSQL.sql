8dedb3179aff70907821d530fe3a66ada4c8efca
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-13 11:15:37 +0300

    DB projects are prepared

---
+++ Queries/Week2/StringManipulationsInSQL.sql
@@ -0,0 +1,6 @@
+/*Test Functions are only used  for the display options, does not modify the table */
+
+select * from employees;
+select email from employees;
+/*Concatenation operator --> || */
+select email||'@gmail.com' from employees;
