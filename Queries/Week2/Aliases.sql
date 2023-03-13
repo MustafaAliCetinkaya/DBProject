424053c74a8a4ba6ff4e7e2b4e1323d43b77ea18
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-13 16:27:01 +0300

    Query projects are done by using Oracle DB

---
+++ Queries/Week2/Aliases.sql
@@ -0,0 +1,10 @@
+--Aliases: used to give temporary name (nickname, codename) to tables and columns
+
+-- For column: we use as keyword. For table: enter a space then name
+
+select first_name||' '||last_name as "FULL NAME" from employees;
+select lower(email||'@gmail.com') as "Email Addresses" from employees;
+
+--Table aliases
+select * from employees;
+select * from employees, job_history;
