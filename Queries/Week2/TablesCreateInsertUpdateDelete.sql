4bfdc64ab678ef2ee36900a4500adff60f999b63
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-13 18:41:47 +0300

    Query projects are done by using Oracle DB

--- Queries/Week2/TablesCreateInsertUpdateDelete.sql
+++ Queries/Week2/TablesCreateInsertUpdateDelete.sql
@@ -9,4 +9,12 @@ create table Customer (
 INSERT INTO Customer (FirstName, LastName, City, Country, Phone)
 VALUES ('Craig', 'Smith', 'New York', 'USA', '1-01-993 7800');
 
+
+INSERT INTO Customer (FirstName, LastName, City, Country, Phone)
+VALUES ('Ali', 'Can', 'New Jersey', 'USA', '1-01-993 9900');
+
+
+INSERT INTO Customer (FirstName, LastName, City, Country, Phone)
+VALUES ('Pella', 'Kar', 'Paris', 'France', '1-01-993 1100');
+
 select * from Customer;
