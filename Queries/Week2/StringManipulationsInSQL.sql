928b8e6b18fd8a26e9beb9730da2468696d2d6d3
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-13 12:43:18 +0300

    DB projects are prepared

--- Queries/Week2/StringManipulationsInSQL.sql
+++ Queries/Week2/StringManipulationsInSQL.sql
@@ -2,6 +2,27 @@
 
 select * from employees;
 select email from employees;
+
 /*Concatenation operator --> || */
 select email||'@gmail.com' from employees;
 select first_name||' '||last_name from employees;
+
+/*concat (value1,value2)*/
+select concat(email,'@gmail.com') from employees;
+select concat(first_name,' ') from employees;
+select concat(concat(first_name,' '),last_name) from employees;
+
+/*Upper () and Lower () changes values to uppercase and lowercase*/
+select lower(first_name) from employees;
+select upper(first_name) from employees;
+
+/*INITCAP() converts only the initial letter to uppercase */
+select initcap(email) from employees;
+
+/*Length(value) counts the letters of the word*/
+select length(first_name) from employees;
+select first_name, length(first_name) from employees;
+
+/*Replace(ColumnName, OldValue, NewValue)*/
+select first_name from employees;
+select replace(first_name,'El','Al') from employees;
