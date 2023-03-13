d0777fe336b437924e60841738994880f9681ff9
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-13 22:19:32 +0300

    Query projects are done by using Oracle DB

--- Queries/Week2/TablesCreateInsertUpdateDelete.sql
+++ Queries/Week2/TablesCreateInsertUpdateDelete.sql
@@ -18,3 +18,19 @@ INSERT INTO Customer (FirstName, LastName, City, Country, Phone)
 VALUES ('Pella', 'Kar', 'Paris', 'France', '1-01-993 1100');
 
 select * from Customer;
+
+create table Supplier(
+                         CompanyName varchar(20) not null,
+                         CompanyID INTEGER PRIMARY KEY,
+                         ContactName varchar(20) not null,
+                         City varchar(20) not null,
+                         Country varchar(20) not null,
+                         Phone INTEGER
+);
+
+INSERT INTO Supplier (CompanyName, CompanyID, ContactName,
+                      City, Country, Phone)
+VALUES ('Broodjes Huis', 1234, 'Henk de Groot',
+        'Amsterdam', 'Netherlands', '5553828847');
+
+select  * from Supplier;
