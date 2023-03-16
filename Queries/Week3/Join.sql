1790fc3ab1a09f91dac81a339d633d60b44fb25b
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-16 08:58:14 +0300

    Query projects are done by using Oracle DB

--- Queries/Week3/Join.sql
+++ Queries/Week3/Join.sql
@@ -63,3 +63,15 @@ where c.ADDRESS_ID is null or a.ADDRESS_ID is null;
 --full join with where: displays the non matching records from left and right table
 select * from CUSTOMER c full join ADDRESS a on c.ADDRESS_ID=a.ADDRESS_ID
 where c.ADDRESS_ID is null or a.ADDRESS_ID is null;
+
+--get me firstname,lastname and department name, city,country_name for all employees;
+select  * from LOCATIONS;
+select  * from COUNTRIES;
+select  * from DEPARTMENTS;
+select first_name , last_name , department_name, city,country_name
+from employees e join departments d
+                      on e.department_id = d.department_id
+                 join locations L
+                      on d.location_id = l.location_id
+                 join countries C
+                      on c.country_id = l.country_id;
