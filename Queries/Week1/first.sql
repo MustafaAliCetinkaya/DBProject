a5e68f3ff58bc13fe6ce135221eff040bad71ef9
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-21 16:29:53 +0300

    Query projects are done by using Oracle DB

--- Queries/Week1/first.sql
+++ Queries/Week1/first.sql
@@ -13,18 +13,27 @@ select EMPLOYEE_ID, DEPARTMENTS.DEPARTMENT_ID from DEPARTMENTS, EMPLOYEES;
 select distinct SALARY from EMPLOYEES;
 select  DISTINCT MAX_SALARY from JOBS;
 
-/*select * from employees;
--- reads all column from employees table
-select * from departments;
--- reads all column from departments table
-select first_name from employees;
---get only firstname from employees table
+
+/*This query will return a table with three columns: FIRST_NAME, HIRE_DATE, and EMPLOYEE_ID, and only the rows where the EMPLOYEE_ID is not equal to 111 will be included in the result set.*/
+select FIRST_NAME, HIRE_DATE, EMPLOYEE_ID from EMPLOYEES
+where  EMPLOYEE_ID <> 111;
+--employee ID of all employees except the one with employee ID 111:
+
+select FIRST_NAME, SALARY from EMPLOYEES
+where SALARY >= 5000 and SALARY <= 10000;
+
+select FIRST_NAME, SALARY from EMPLOYEES
+where  SALARY between 5000 and 10000;
+
 --display city names
 select city from locations;
+
 --get me first_name,last_name and salary
 select first_name,last_name,salary from employees;
+
 --get me street address and postal code
 SELECT street_address, postal_code
 FROM locations;
+
 select distinct first_name from employees;
---remove duplicates*/
+--remove duplicates
