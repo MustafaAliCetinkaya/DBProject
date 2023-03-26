80118d8f3b42f5df79304c9ce692dffe50608cee
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-26 12:12:35 +0300

    Query projects are done by using Oracle DB

---
+++ Queries/Week4/Review1.sql
@@ -0,0 +1,97 @@
+
+-- 1. get me all data from employees table
+select * from EMPLOYEES;
+
+-- 2. get me first_name, phone_number and salary from employees
+select FIRST_NAME, PHONE_NUMBER, SALARY from EMPLOYEES;
+
+-- 3. get me unique job_id from _____ table
+select distinct JOB_ID from EMPLOYEES;
+
+-- 4. get me all ST_CLERK first name from employee table
+select FIRST_NAME, JOB_ID from EMPLOYEES
+where JOB_ID = 'ST_CLERK';
+
+-- 5. get me postal_code from JP,UK,CA
+select COUNTRY_ID, POSTAL_CODE from LOCATIONS
+where COUNTRY_ID = 'JP' or COUNTRY_ID = 'UK' or COUNTRY_ID = 'CA';
+
+select COUNTRY_ID, POSTAL_CODE from LOCATIONS
+where COUNTRY_ID in ('UK','JP','CA');
+
+-- 6. get me city not in CH,US
+SELECT CITY, COUNTRY_ID FROM LOCATIONS
+WHERE COUNTRY_ID NOT IN ('CH','US');
+
+-- 7. get me job_title whose min_salary is more than 4000 and less than 9000
+SELECT JOB_TITLE, MIN_SALARY FROM JOBS
+WHERE MIN_SALARY BETWEEN 4000 AND 9000;
+
+-- 8. get me null state_province in locations table
+SELECT STATE_PROVINCE FROM LOCATIONS
+WHERE STATE_PROVINCE IS NULL ;
+
+-- 9. get me NOT NULL manager_id in departments table
+SELECT MANAGER_ID FROM DEPARTMENTS
+WHERE MANAGER_ID IS NOT NULL ;
+
+-- 10. get me employee_id and start_date who starts in 2001
+SELECT JOB_ID, HIRE_DATE FROM EMPLOYEES
+WHERE HIRE_DATE LIKE '%2005';
+
+-- 11. get me all employee first_name who works in some CLERK position
+SELECT FIRST_NAME, JOB_ID FROM EMPLOYEES
+WHERE JOB_ID LIKE '%CLERK';
+
+-- 12. get me email whose email is 6 letter and 4th letter of email is E
+SELECT EMAIL FROM EMPLOYEES
+WHERE EMAIL LIKE '___E__';
+
+-- 13. get me last_name according to their hire_date
+SELECT LAST_NAME,HIRE_DATE FROM EMPLOYEES
+ORDER BY HIRE_DATE;
+
+-- 15. get me the count of manager_id from departments
+select count(MANAGER_ID) from EMPLOYEES;
+
+-- 19. get me maximum salary of each department_id
+select DEPARTMENT_ID, max(salary) from EMPLOYEES
+group by DEPARTMENT_ID
+order by max(SALARY);
+
+-- 21. get me salary information( min, max, sum, avg) of each job_id
+select JOB_ID, min(SALARY), max(SALARY), sum(SALARY), avg(SALARY) from EMPLOYEES
+group by JOB_ID;
+
+-- 23. get me department_id who has more than 10 employee
+select DEPARTMENT_ID, count(FIRST_NAME) from EMPLOYEES
+group by DEPARTMENT_ID
+having count(FIRST_NAME) > 10;
+
+-- 24. get me which job_id has average commission_pct over 20%
+select JOB_ID, avg(COMMISSION_PCT) from EMPLOYEES
+group by JOB_ID
+having avg(COMMISSION_PCT) > 0.2;
+
+-- 5. get me first name and department name in same table
+select FIRST_NAME, DEPARTMENT_NAME from EMPLOYEES
+                                            inner join DEPARTMENTS
+                                                       on   DEPARTMENTS.DEPARTMENT_ID = EMPLOYEES.DEPARTMENT_ID;
+
+-- 6. get me job id and manager id in one table
+select JOB_ID, DEPARTMENTS.MANAGER_ID from EMPLOYEES
+                                               inner join DEPARTMENTS on EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID;
+
+-- 7. get me department names and end date( if any employee quit)
+select DEPARTMENT_NAME, END_DATE from DEPARTMENTS
+                                          left join JOB_HISTORY on DEPARTMENTS.DEPARTMENT_ID = JOB_HISTORY.DEPARTMENT_ID
+where END_DATE is not null ;
+
+--11.get me department name, city in same table
+select DEPARTMENT_NAME, CITY from DEPARTMENTS
+                                      left join LOCATIONS L on L.LOCATION_ID = DEPARTMENTS.LOCATION_ID;
+
+
+
+
+
---
+++ Queries/Week4/Review2.sql
@@ -0,0 +1,273 @@
+-- in statement
+select FIRST_NAME
+from EMPLOYEES
+where EMPLOYEE_ID = 107
+   or EMPLOYEE_ID = 102
+   or EMPLOYEE_ID = 103
+   or EMPLOYEE_ID = 104;
+
+select FIRST_NAME
+from EMPLOYEES
+where EMPLOYEE_ID in (107, 102, 103, 104);
+
+select CITY, COUNTRY_ID
+from LOCATIONS
+where COUNTRY_ID in ('UK', 'US');
+
+SELECT CITY, COUNTRY_ID
+FROM LOCATIONS
+WHERE COUNTRY_ID NOT IN ('UK', 'US');
+
+-- ORDER BY
+SELECT SALARY
+from EMPLOYEES;
+
+select SALARY
+from EMPLOYEES
+order by SALARY desc;
+
+select SALARY
+from EMPLOYEES
+order by SALARY asc;
+
+select SALARY
+from EMPLOYEES
+order by SALARY;
+
+select FIRST_NAME
+from EMPLOYEES
+order by SALARY;
+
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+order by FIRST_NAME;
+
+select FIRST_NAME, LAST_NAME
+from EMPLOYEES
+order by FIRST_NAME asc, LAST_NAME desc;
+
+select FIRST_NAME, SALARY
+from EMPLOYEES
+order by FIRST_NAME asc, SALARY desc;
+
+-- like
+
+select FIRST_NAME
+from EMPLOYEES
+where FIRST_NAME like 'Da%';
+
+select LAST_NAME
+from EMPLOYEES
+where LAST_NAME like '%e';
+
+select LAST_NAME
+from EMPLOYEES
+where LAST_NAME like '_____';
+
+select FIRST_NAME
+from EMPLOYEES
+where FIRST_NAME like '__a__';
+
+select FIRST_NAME
+from EMPLOYEES
+where FIRST_NAME like 'D__%';
+
+SELECT CITY, COUNTRY_ID
+FROM LOCATIONS
+WHERE COUNTRY_ID like 'U%';
+
+--count
+select COUNT(JOB_ID)
+from JOB_HISTORY;
+
+select JOB_ID
+from JOB_HISTORY;
+
+select count(*)
+from LOCATIONS;
+
+select *
+from LOCATIONS;
+
+select count(POSTAL_CODE)
+from LOCATIONS;
+
+select count(COUNTRY_ID)
+from LOCATIONS;
+
+select count(distinct COUNTRY_ID)
+from LOCATIONS;
+
+select Distinct COUNTRY_ID
+from LOCATIONS;
+
+-- aggregate functions
+-- min
+select min(SALARY)
+from EMPLOYEES;
+select min(FIRST_NAME)
+from EMPLOYEES;
+select FIRST_NAME
+from EMPLOYEES
+order by FIRST_NAME;
+
+-- max
+select max(SALARY)
+from EMPLOYEES;
+select max(LAST_NAME)
+from EMPLOYEES;
+select max(HIRE_DATE)
+from EMPLOYEES;
+select HIRE_DATE
+from EMPLOYEES
+order by HIRE_DATE;
+
+
+--sum
+select sum(SALARY)
+from EMPLOYEES;
+
+select sum(SALARY)
+from EMPLOYEES
+where JOB_ID = 'IT_PROG';
+
+-- avg
+select avg(SALARY)
+from EMPLOYEES;
+select round(avg(SALARY))
+from EMPLOYEES;
+select round(avg(SALARY), 2)
+from EMPLOYEES;
+select round(avg(SALARY), 0)
+from EMPLOYEES;
+
+-- group by
+select JOB_ID
+from EMPLOYEES;
+
+select JOB_ID
+from EMPLOYEES
+group by JOB_ID;
+
+select JOB_ID, count(FIRST_NAME)
+from EMPLOYEES
+group by JOB_ID;
+
+select JOB_ID, sum(SALARY)
+from EMPLOYEES
+group by JOB_ID;
+
+select JOB_ID, avg(SALARY)
+from EMPLOYEES
+group by JOB_ID;
+
+select JOB_ID, avg(SALARY)
+from EMPLOYEES
+group by JOB_ID
+having avg(SALARY) > 8000;
+
+select DEPARTMENT_ID, count(FIRST_NAME)
+from EMPLOYEES
+group by DEPARTMENT_ID
+order by count(FIRST_NAME);
+
+-- difference between having and where
+-- where is put condition before group, applies to individual rows
+-- having is put condition after group, applies to grouped rows
+
+-- row number
+select FIRST_NAME
+from EMPLOYEES
+where ROWNUM <= 10;
+
+-- subquery
+select avg(SALARY)
+from EMPLOYEES;
+
+select FIRST_NAME, SALARY
+from EMPLOYEES
+where SALARY > (select avg(SALARY) from EMPLOYEES);
+
+select FIRST_NAME, SALARY
+from EMPLOYEES
+where SALARY < (select max(salary) from EMPLOYEES);
+
+select max(salary)
+from EMPLOYEES
+where SALARY < (select max(salary) from EMPLOYEES);
+
+select SALARY
+from (select salary from employees order by salary desc)
+where ROWNUM < 11;
+
+
+-- string fucntions
+-- concatination
+select FIRST_NAME || ' ' || LAST_NAME
+from EMPLOYEES;
+
+-- lower or upper
+select FIRST_NAME
+from EMPLOYEES;
+select upper(FIRST_NAME)
+from EMPLOYEES;
+select lower(LAST_NAME)
+from EMPLOYEES;
+
+-- lenght
+select FIRST_NAME, length(FIRST_NAME)
+from EMPLOYEES
+where length(FIRST_NAME) < 6;
+
+-- substr
+select LAST_NAME, substr(LAST_NAME, 1, 3)
+from EMPLOYEES;
+select CITY, substr(CITY, 1, 2)
+from LOCATIONS;
+
+-- intcap
+select initcap(lower(FIRST_NAME))
+from EMPLOYEES;
+
+-- view
+create view email as
+select FIRST_NAME || '.' || LAST_NAME || '@gmail.com' as fullEmail
+from EMPLOYEES;
+
+select FIRST_NAME || '.' || LAST_NAME || '@gmail.com'
+from EMPLOYEES;
+
+
+-- join
+-- inner join
+select Jobs.JOB_ID, START_DATE, MAX_SALARY
+from JOBS
+         inner join JOB_HISTORY on JOBS.JOB_ID = JOB_HISTORY.JOB_ID;
+
+-- left outer join
+select Jobs.JOB_ID, START_DATE, MAX_SALARY
+from JOBS
+         left outer join JOB_HISTORY on JOBS.JOB_ID = JOB_HISTORY.JOB_ID;
+
+-- join with where
+select Jobs.JOB_ID, START_DATE, MAX_SALARY
+from JOBS
+         left outer join JOB_HISTORY on JOBS.JOB_ID = JOB_HISTORY.JOB_ID
+where START_DATE is not null;
+
+
+-- right outer join
+select Jobs.JOB_ID, START_DATE, MAX_SALARY
+from JOBS
+         right outer join JOB_HISTORY on JOBS.JOB_ID = JOB_HISTORY.JOB_ID;
+
+
+-- full outer join
+select Jobs.JOB_ID, START_DATE, MAX_SALARY
+from JOBS
+         full outer join JOB_HISTORY on JOBS.JOB_ID = JOB_HISTORY.JOB_ID;
+
+-- self join
+select e1.EMPLOYEE_ID, e1.FIRST_NAME, e1.EMAIL, e2.MANAGER_ID from EMPLOYEES e1
+                                                                       join EMPLOYEES e2
+                                                                            on e1.EMPLOYEE_ID = e2.MANAGER_ID;
